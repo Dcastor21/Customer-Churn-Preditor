f2990c81f04b137c3f265d4eab07de3f7f76e267
Darnel Castor <darnelcastor2@gmail.com>
2024-11-14 23:18:30 -0500

    added utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
 import plotly.graph_objects as go
+
+def create_gauge_chart(probability):
+    if probability < 0.3:
+        color = 'green'
+    elif probability < 0.6:
+        color = 'yellow'
+    else:
+        color = 'red'
+        
+    # Create a gauge chart
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number",
+    ))
